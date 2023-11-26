b7f0fb45b450d5f0476d1269e22a7485e16868ca
pupov <pupov435@gmail.com>
2023-11-26 19:42:39 +0700

    - Implemented profile logic - Implemented UsersController

---
+++ BlogAPI/Models/Request/UserEditRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogAPI.Models.Request;
+
+public record UserEditRequest(string Email, string FullName, string BirthDate, string Gender, string PhoneNumber)
+{
+    public UserEditRequest() : this(string.Empty, string.Empty, string.Empty, Enums.Gender.Male.ToString(), string.Empty)
+    {
+    }
+}
+
---
+++ BlogAPI/services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using BlogAPI.Models;
+using BlogAPI.Models.Request;
+using BlogAPI.Models.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAPI.services.Interfaces;
+
+public interface IUserService
+{
+    public Task<UserProfileResponse> GetUserProfileAsync();
+    public Task<IActionResult> UpdateUserProfileAsync(UserEditRequest userEditRequest);
+}
