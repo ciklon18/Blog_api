a93110601e4b7ec475cb40ea2c673e9691dad9e5
pupov <pupov435@gmail.com>
2023-12-01 14:32:19 +0700

    - Added 2 requests for posts in CommunityService - Added Post entity, Post request, Post responses

---
+++ BlogAPI/Entities/Post.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace BlogAPI.Entities;
+
+[Table("posts")]
+public class Post
+{
+    [Key] [Column("id")] public Guid Id { get; set; }
+
+    [Column("create_time")] public DateTime CreateTime { get; set; }
+
+    [Column("title")]
+    [Required(AllowEmptyStrings = false)]
+    public required string Title { get; set; }
+
+    [Column("description")]
+    [Required(AllowEmptyStrings = false)]
+    public required string Description { get; set; }
+
+    [Column("reading_time")] public int ReadingTime { get; set; }
+
+    [Column("image")] public string? Image { get; set; }
+
+    [Column("author_id")] public Guid AuthorId { get; set; }
+
+    [Column("author")]
+    [Required(AllowEmptyStrings = false)]
+    public required string Author { get; set; }
+
+    [Column("community_id")] public Guid? CommunityId { get; set; }
+
+    [Column("community_name")]
+    [Required(AllowEmptyStrings = false)]
+    public required string CommunityName { get; set; }
+
+    [Column("address_id")] public Guid? AddressId { get; set; }
+
+    [Column("likes")] public int Likes { get; set; }
+
+    [Column("has_like")] public bool HasLike { get; set; }
+
+    [Column("comments_count")] public int CommentsCount { get; set; }
+
+    
+    [JsonIgnore]
+    public ICollection<PostTag> PostTags { get; set; } = new List<PostTag>();
+}
--- BlogAPI/services/Interfaces/ICommunityService.cs
+++ BlogAPI/services/Interfaces/ICommunityService.cs
@@ -1,14 +1,18 @@
-﻿using BlogAPI.Models.Response;
+﻿using BlogAPI.Enums;
+using BlogAPI.Models.Request;
+using BlogAPI.Models.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogAPI.services.Interfaces;
 
 public interface ICommunityService
 {
-    List<CommunityResponse> GetCommunityList();
+    Task<List<CommunityResponse>> GetCommunityList();
     Task<List<CommunityUserResponse>> GetMyCommunityList();
     Task<CommunityFullResponse> GetCommunityInfo(Guid id);
-    Task<OkObjectResult>  GetCommunityUserRole(Guid id);
+    Task<PostPagedListResponse> GetCommunityPosts(Guid communityId, List<Guid> tagIds, PostSorting sort, int page, int pageSize);
+    Task<IActionResult> PostCommunityPost(Guid communityId, PostRequest postRequest);
+    Task<OkObjectResult> GetCommunityUserRole(Guid id);
     Task<IActionResult> SubscribeUserToCommunity(Guid communityId);
     Task<IActionResult> UnsubscribeUserToCommunity(Guid id);
 }
