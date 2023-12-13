6fca7a1cb118c5bcca3ab603f5afa97b803d6149
pupov <pupov435@gmail.com>
2023-12-13 17:18:35 +0700

    - simple updates

--- BlogAPI/Enums/Gender.cs
+++ BlogAPI/Enums/Gender.cs
@@ -1,5 +1,8 @@
-﻿namespace BlogAPI.Enums;
+﻿using System.Text.Json.Serialization;
 
+namespace BlogAPI.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Gender
 {
     Male,
---
+++ BlogAPI/Migrations/20231212143853_changed_token_value_from_email_to_userId.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BlogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class changed_token_value_from_email_to_userId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.CreateTable(
+                name: "refresh_tokens",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    token = table.Column<string>(type: "text", nullable: false),
+                    expires = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    revoked = table.Column<bool>(type: "boolean", nullable: false),
+                    userId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_refresh_tokens", x => x.id);
+                });
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropTable(
+                name: "refresh_tokens");
+        }
+    }
+}
